5e451fac9ac722a9c97653f4f8f75c46f5f75299
Ancelad <i_am_pomidorchik@mail.ru>
2017-01-09 13:22:43 +0500

    russina translation

---
+++ translations/Talefish-ru.ts
@@ -0,0 +1,333 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" sourcelanguage="en" version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Talefish</source>
+        <comment>header</comment>
+        <translation>О Talefish</translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <comment>section header</comment>
+        <translation>Поддержка</translation>
+    </message>
+    <message>
+        <source>Discuss or click &quot;thanks&quot; at talk.maemo.org:</source>
+        <translation>Обсудить или поблагодарить на talk.maemo.org</translation>
+    </message>
+    <message>
+        <source>TMO thread</source>
+        <translation>Тема на ТМО</translation>
+    </message>
+    <message>
+        <source>GitHub page</source>
+        <translation>Страница GitHub</translation>
+    </message>
+    <message>
+        <source>You can also submit translations at Transifex, if you don&apos;t like GitHub:</source>
+        <translation>Также вы можете перевести приложение на Transifex, если вам не нравится GitHub:</translation>
+    </message>
+    <message>
+        <source>Transifex page</source>
+        <translation>Страница Transifex</translation>
+    </message>
+    <message>
+        <source>Or buy me a beer if you really feel like it:</source>
+        <translation>Если вам действительно нравится приложение, то вы можете купить мне немножко пива:</translation>
+    </message>
+    <message>
+        <source>PayPal donation</source>
+        <translation>Пожертвование через PayPal</translation>
+    </message>
+    <message>
+        <source>Thanks!</source>
+        <comment>header</comment>
+        <translation>Спасибо!</translation>
+    </message>
+    <message>
+        <source>Thanks to all users suggesting things and everyone helping me out!</source>
+        <translation>Спасибо всем пользователям, принявшим участие в проекте!</translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <comment>header</comment>
+        <translation>Переводчики</translation>
+    </message>
+    <message>
+        <source>A big thank you to the translators:</source>
+        <translation>Огромная благодарность переводчикам:</translation>
+    </message>
+    <message>
+        <source>Talefish is a directory based audio book player written mainly in QML. It&apos;s licensed under GPL2 and made by John Gibbon with the help of the community.</source>
+        <translation>Talefish представляет собой плеер для аудиокниг, написанный, преимущественно, на QML. Главной отличительной особенностью является возможность воспроизведения аудиокниг из конкретной директории. Данное приложение создано Джоном Гиббоном (John Gibbon) при участии сообщества и распространется под лицензией GPL2.</translation>
+    </message>
+    <message>
+        <source>Project stars, pull requests or bug reports are welcome on GitHub:</source>
+        <translation>Чтобы оценить приложение, улучшить его или добавить отчет об ошибках, посетите страницу на GitHub:</translation>
+    </message>
+</context>
+<context>
+    <name>FolderSelector</name>
+    <message>
+        <source>Sort by Name</source>
+        <translation>Сортировать по названию</translation>
+    </message>
+    <message>
+        <source>Sort by Type</source>
+        <translation>Сортировать по типу</translation>
+    </message>
+    <message>
+        <source>Sort by Last Modified</source>
+        <translation>Сортировать по дате изменения</translation>
+    </message>
+    <message>
+        <source>Open Directory</source>
+        <translation>Открыть директорию</translation>
+    </message>
+    <message numerus="yes">
+        <source>click file to open it or accept dialog to open %1 file(s)</source>
+        <comment>only shown when there are files</comment>
+        <translation>кликните по файлу, чтобы открыть его или согласитесь с предложением открыть %1 файла(ов)</translation>
+    </message>
+    <message>
+        <source>Enqueue Directory</source>
+        <translation>Добавить директорию в очередь</translation>
+    </message>
+    <message numerus="yes">
+        <source>Click file to enqueue or accept dialog to enqueue %1 file(s)</source>
+        <comment>only shown when there are files</comment>
+        <translation>Кликните по файлу, чтобы поставить его в очередь его или согласитесь с предложением поставить в очередь %1 файла(ов)</translation>
+    </message>
+    <message>
+        <source>Progress will not be saved for reopening enqueued files.</source>
+        <translation>Прогресс не сохранится для повторного открытия файлов в очереди.</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsPage</name>
+    <message>
+        <source>Options</source>
+        <comment>header</comment>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Playback</source>
+        <comment>section header</comment>
+        <translation>Воспроизведение</translation>
+    </message>
+    <message>
+        <source>Playback Speed: %1%</source>
+        <comment>percent</comment>
+        <translation>Скорость воспроизведения: %1%</translation>
+    </message>
+    <message>
+        <source>Play next Track automatically</source>
+        <translation>Воспроизводить следующий трек автоматически</translation>
+    </message>
+    <message>
+        <source>Short Skip duration</source>
+        <translation>Продолжительность короткой перемотки</translation>
+    </message>
+    <message>
+        <source>Long Skip duration</source>
+        <translation>Продолжительность длинной перемотки</translation>
+    </message>
+    <message>
+        <source>Search best cover image for each file</source>
+        <translation>Поиск наилучшей обложки для каждого файла</translation>
+    </message>
+    <message>
+        <source>Do Animations</source>
+        <translation>Анимация</translation>
+    </message>
+    <message>
+        <source>Player Page</source>
+        <comment>section header</comment>
+        <translation>Анимация на главной странице</translation>
+    </message>
+    <message>
+        <source>Display directory name</source>
+        <translation>Показывать название директории</translation>
+    </message>
+    <message>
+        <source>Display playlist progress</source>
+        <translation>Показывать прогресс воспроизведения</translation>
+    </message>
+    <message>
+        <source>Swipe Cover (or above Title) to skip Tracks</source>
+        <translation>Переключение треков жестом по миниатюре (или по названию трека)</translation>
+    </message>
+    <message>
+        <source>Cassette shows progress of</source>
+        <translation>Анимация кассеты для отображения прогресса</translation>
+    </message>
+    <message>
+        <source>track</source>
+        <translation>трека</translation>
+    </message>
+    <message>
+        <source>directory</source>
+        <translation>директории</translation>
+    </message>
+    <message>
+        <source>Tape Animations</source>
+        <comment>section header</comment>
+        <translation>Анимация кассеты</translation>
+    </message>
+    <message>
+        <source>Player Page Animation</source>
+        <translation>Анимация главной страницы</translation>
+    </message>
+    <message>
+        <source>App-Cover Animation</source>
+        <translation>Анимация миниатюры</translation>
+    </message>
+    <message>
+        <source>Open Files</source>
+        <comment>section header</comment>
+        <translation>Открыть файлы</translation>
+    </message>
+    <message>
+        <source>Show &quot;Enqueue&quot; Pulley</source>
+        <translation>Показывать &quot;Очередь&quot; в вытягиваемом меню</translation>
+    </message>
+    <message>
+        <source>Miscellaneous</source>
+        <comment>section header</comment>
+        <translation>Прочее</translation>
+    </message>
+    <message>
+        <source>Save progress periodically</source>
+        <translation>Периодическое сохранение прогресса</translation>
+    </message>
+    <message>
+        <source>If disabled, the current playback state will only be saved when the app cleanly exits. Otherwise, It will save the progress every few seconds.</source>
+        <translation>Если выключено, то текущий прогресс воспроизведения будет сохранен только при полном выходе из приложения. В противном случае прогресс сохраняется каждые несколько секунд.</translation>
+    </message>
+    <message>
+        <source>Verbose logging enabled</source>
+        <translation>Подробное ведение журнала</translation>
+    </message>
+    <message>
+        <source>Outputs a lot of things in the Background, can get a bit slower. View output by opening Talefish via Terminal.</source>
+        <translation>Вывод журнала работы в фоновом режиме, при этом приложение может работать немного медленнее. Для просмотра журнала запустите Talefish из терминала.</translation>
+    </message>
+    <message>
+        <source>Sort directory naturally while scanning</source>
+        <translation>Непосредственная сортировка директорий во время сканирования</translation>
+    </message>
+    <message>
+        <source>Enable if you have files numbered without leading zeroes. Only applicable when sorted by name.</source>
+        <translation>Включение требуется, если номера ваших файлов не начинаются с нуля. Применяется только при сортировке по названию.</translation>
+    </message>
+    <message>
+        <source>Use head phone buttons/Bluetooth to control Talefish</source>
+        <translation>Использовать кнопки наушников или Bluetooth для управления</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <comment>pulley</comment>
+        <translation>О программе</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <source>Options</source>
+        <comment>pulley</comment>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Playlist</source>
+        <comment>pulley</comment>
+        <translation>Плейлист</translation>
+    </message>
+    <message>
+        <source>Enqueue</source>
+        <comment>pulley</comment>
+        <translation>Очередь</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <comment>pulley</comment>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <source>Nothing to play</source>
+        <translation>Нет ничего для воспроизведения</translation>
+    </message>
+    <message>
+        <source>Open Files by pulling down.</source>
+        <translation>Потяните вниз, чтобы открыть файлы.</translation>
+    </message>
+    <message>
+        <source>Rewind Track</source>
+        <translation>Перемотка трека</translation>
+    </message>
+    <message>
+        <source>Previous Track</source>
+        <translation>Предыдущий трек</translation>
+    </message>
+    <message>
+        <source>Next Track</source>
+        <translation>Следующий трек</translation>
+    </message>
+    <message>
+        <source>%1 / %2 (File %L3 of %L4)</source>
+        <comment>formatted file/directory durations, then file number/count )</comment>
+        <translation>%1 / %2 (Файл %L3 из %L4)</translation>
+    </message>
+    <message numerus="yes">
+        <source>%1% played in Total</source>
+        <comment>directory progress</comment>
+        <translation>%1% воспроизведено от всего</translation>
+    </message>
+    <message>
+        <source>[No Cover]</source>
+        <translation>[Нет обложки]</translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistPage</name>
+    <message numerus="yes">
+        <source>%L1 file(s) opened</source>
+        <translation>%L1 файл(ов) открыт(о)</translation>
+    </message>
+    <message>
+        <source>%1 of %2 played (track %3)</source>
+        <translation>%1 из %2 воспроизведено (трек %3)</translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistView</name>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>scanDialog</name>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message numerus="yes">
+        <source>%1 Files</source>
+        <comment>header</comment>
+        <translation>%1 файлов</translation>
+    </message>
+    <message>
+        <source>no Duration</source>
+        <comment>if file duration &lt; 1; instead of duration</comment>
+        <translation>Нет продолжительности</translation>
+    </message>
+    <message>
+        <source>Reading Durations</source>
+        <translation>Оценка продолжительности</translation>
+    </message>
+    <message>
+        <source>Reading Durations and matching Cover files</source>
+        <translation>Оценка продолжительности и соотнесение обложек</translation>
+    </message>
+</context>
+</TS>
